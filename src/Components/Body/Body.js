346a53dd85b927c89558cf0425a31b584ba474d0
Jean-Paul <Jean.paul.bassil@outlook.com>
2023-02-14 19:39:58 +0200

    generated the pictures on click using state

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -6,7 +6,6 @@ export default function Body(){
     return (
         <div className='body'>
             <Input />
-            <img src='' alt='meme' className='meme'/>
         </div>
     )
 }
--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,12 +1,28 @@
 import './Input.css';
 import Button from '../Button/Button';
+import data from '../../memesData';
+import React from 'react';
 
-export default function Input(){
+
+export default function Input() {
+
+    const [currentURL, changeURL] = React.useState(""); 
+    
+    
+    function logRandMeme(){
+        const memes = data.data.memes;
+        const randMeme = memes[Math.floor(Math.random() * memes.length)];
+        changeURL(randMeme.url);
+    }
+    
     return (
         <div className='input'>
             <input />
             <input />
-            <Button />
+            <div className='btn' onClick={logRandMeme}>
+                <Button />
+            </div>
+            <img className="meme" src={currentURL}/>
         </div>
-    )
+    ) 
 }
