393b61a2a3ed4f6a5dff35502c5af99784c56c09
Jean-Paul <Jean.paul.bassil@outlook.com>
2023-02-15 10:57:15 +0200

    added state management

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -6,23 +6,33 @@ import React from 'react';
 
 export default function Input() {
 
-    const [currentURL, changeURL] = React.useState(""); 
+    const [currentMeme, setCurrentMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        img: "https://i.imgflip.com/1bij.jpg"
+    }); 
     
-    
+    const [allMemeImages, setAllMemeImages] = React.useState(data.data.memes);
     function logRandMeme(){
-        const memes = data.data.memes;
-        const randMeme = memes[Math.floor(Math.random() * memes.length)];
-        changeURL(randMeme.url);
+        const randMeme = allMemeImages[Math.floor(Math.random() * allMemeImages.length)];
+        console.log(randMeme.url);
+        setCurrentMeme(prevCurrentMeme => {
+            return {
+                ...prevCurrentMeme,
+                img: randMeme.url
+            }
+        });
+        console.log(currentMeme.img);
     }
     
     return (
         <div className='input'>
-            <input />
-            <input />
+            <input placeholder='Top Text'/>
+            <input placeholder='Bottom Text'/>
             <div className='btn' onClick={logRandMeme}>
                 <Button />
             </div>
-            <img className="meme" src={currentURL}/>
+            <img className="meme" src={currentMeme.img}/>
         </div>
     ) 
 }
